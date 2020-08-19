10be15b9193f546bfe717089249f45be7e615076
Blen Wondimagegn <mekuriablen@gmail.com>
2020-08-19 01:23:10 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,29 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+def speak_to_grandma(phase)
+  if phase != phase.upcase
+    'HUH?! SPEAK UP, SONNY!'
+  elsif phase == 'I LOVE YOU GRANDMA!'
+    'I LOVE YOU TOO PUMPKIN!'
+  elsif phase == 'WHAT DID YOU EAT TODAY?'
+     'NO, NOT SINCE 1938!'
+  else phase == "WHAT?"
+     'NO, NOT SINCE 1938!'  
+  end
+end 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+  
